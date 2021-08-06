3cedd41605257c372a56ff3d30aad174983d6ccc
Dexter Vorbe <dextervorbe@ymail.com>
2021-08-06 00:55:22 -0400

    Added Heroku deployment

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static('public'));
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
